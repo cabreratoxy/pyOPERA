cbec9e76576cbe875707c34ea3133e53fd0f54d5
cabreratoxy <mcabrera1001@gmail.com>
2022-12-02 23:37:27 -0500

    Formatted black conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,31 +5,31 @@
 
 import pathlib
 import sys
+
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyOPERA'
-copyright = '2022, Manuel Cabrera'
-author = 'Manuel Cabrera'
-release = '0.1.0'
+project = "pyOPERA"
+copyright = "2022, Manuel Cabrera"
+author = "Manuel Cabrera"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
