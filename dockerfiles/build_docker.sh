36af80c63b4150a10331fe92a5c7081144115d55
cabreratoxy <mcabrera1001@gmail.com>
2022-12-10 13:09:42 -0500

    Used the correct Dockerfile directory

--- dockerfiles/build_docker.sh
+++ dockerfiles/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0 .
-docker push -t cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0
+docker push cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0
