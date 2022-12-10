010944925f6a1161859d1776bdee1cf5e5a8fb1e
cabreratoxy <mcabrera1001@gmail.com>
2022-12-10 13:11:25 -0500

    Provided correct directory for dockerfile in bsh

--- dockerfiles/build_docker.sh
+++ dockerfiles/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0 .
+docker build -t cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0 -f dockerfiles/Dockerfile
 docker push cabreratoxy/pyopera:0.${CIRCLE_BUILD_NUM}.0
